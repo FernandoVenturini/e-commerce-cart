86f776f0208b90ac3e7510636a1e08bda023e4db
Fernando <fernandoventurini9@hotmail.com>
2025-07-16 22:42:37 +0100

    creating shopping cart page #02

--- cart/src/pages/cart/index.tsx
+++ cart/src/pages/cart/index.tsx
@@ -1,7 +1,33 @@
 export function Cart() {
     return (
-        <div className="text-3xl font-bold underline">
-        <h1>Shopping cart page</h1>
+        <div className="w-full max-w-7x1 mx-auto"> 
+        <h1 className="ffont-medium text-2-1 text-center my-4">Meu carrinho</h1>
+
+        <section className="flex items-center justify-between border-b-2 border-gray-300">
+            <img 
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSn1OO7ksAMtaXnVRiG3OW0v6TqHWoR7lvuHw&s" 
+                alt="logo produto" 
+                className="w-28"
+            />
+
+            <strong>Preco: R$1.000</strong>
+
+            <div className="flex items-center justify-center gap-3">
+                <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+                    -
+                </button>
+                2
+                <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+                    +
+                </button>
+            </div>
+
+            <strong className="float-right">
+                Total: R$1.000
+            </strong>
+        </section>
+
+        <p className="font-bold mt-4">Total: R$1.000</p>
         </div>
     );
 };
--- cart/src/pages/home/index.tsx
+++ cart/src/pages/home/index.tsx
@@ -1,7 +1,71 @@
+// importing the cart icon from react-icons
+import { BsCartPlus } from "react-icons/bs";
+
 export function Home() {
     return (
-        <div className="text-3xl font-bold underline">
-        <h1>Cart</h1>
+        <div>
+            <main className="w-full max-w-7x1 px-4 mx-auto">
+                <h1 className="font-bold text-2x1 mb-4 mt-10 text-center">Produtos em alta</h1>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
+
+                    <section className="w-full">
+                        <img
+                            className="w-full rounded-lg max-h-70 mb-2"
+                            src="https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/airpods-4-select-202409_FV1?wid=976&hei=916&fmt=jpeg&qlt=90&.v=WnVKRVRUTFVsYThXaWkydWViL1Q3ZDZGTE9TV3RDcGJJclBqdUtzdTJYYjNHc3NlSmU2dzJyR1kxZEwyTE1neUJkRlpCNVhYU3AwTldRQldlSnpRa0NZZXAxWFNjRXhITDI1RVE5YVpyU0E" 
+                            alt="fone" 
+                        />
+
+                        <p className="font-medium mt-1 mb-2">Airpods Apple</p>
+
+                        <div className="flex gap-3 items-center">
+                            <strong className="text-zinc-700/90">1000</strong>
+                            <button className="bg-zinc-700 p-1 rounded">
+                                <BsCartPlus size={20} color="fff"/>
+                                Adicionar ao carrinho
+                            </button>
+                        </div>
+                    </section>
+
+                    <section className="w-full">
+                        <img
+                            className="w-full rounded-lg max-h-70 mb-2"
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeKRDm0Js78UrlZzPKAj6Wb7XUJZrD5_ONPA&s" 
+                            alt="fone" 
+                        />
+
+                        <p className="font-medium mt-1 mb-2">JBL</p>
+
+                        <div className="flex gap-3 items-center">
+                            <strong className="text-zinc-700/90">1000</strong>
+                            <button className="bg-zinc-700 p-1 rounded">
+                                <BsCartPlus size={20} color="fff"/>
+                                Adicionar ao carrinho
+                            </button>
+                        </div>
+                    </section>
+
+                    <section className="w-full">
+                        <img
+                            className="w-full rounded-lg max-h-70 mb-2"
+                            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzOlW7j3MBOtaXCF1vaOzKMmeRKFNMHlnxpA&s" 
+                            alt="fone" 
+                        />
+
+                        <p className="font-medium mt-1 mb-2">Keyboard mechanical</p>
+
+                        <div className="flex gap-3 items-center">
+                            <strong className="text-zinc-700/90">1000</strong>
+                            <button className="bg-zinc-700 p-1 rounded">
+                                <BsCartPlus size={20} color="fff"/>
+                                Adicionar ao carrinho
+                            </button>
+                        </div>
+                    </section>
+                    
+
+                </div>
+            </main>
         </div>
     );
 };
